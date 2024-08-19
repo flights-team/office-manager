802415a5b92d031de30d045a1a081b212074b90a
Artur <arttturyyy@gmail.com>
2024-08-19 16:24:59 +0200

    initial commit

---
+++ src/main/java/com/artur/office_manager/OfficeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.artur.office_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artur/office_manager/OfficeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artur.office_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
